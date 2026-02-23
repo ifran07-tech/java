3806b78733ab41756377246561b523eadf97e64f
ifran07-tech <mohamedifran49953@gmail.com>
2026-02-23 12:15:18 +0530

    Add files via upload

---
+++ ai2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class ai2
+{
+    public static void main(String[] args) {
+        Scanner m=new Scanner(System.in);
+        System.out.println("Enter A Number :");
+        int a=m.nextInt();
+        char value='';
+        for(int i=0;i<hex a.length();i++);
+        {
+            value=hex a.cha
+        }
+
+
+    }
+}
+
